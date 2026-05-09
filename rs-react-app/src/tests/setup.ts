2d61486ecd5acc69c7cef50eef2eecb1194ca5e5
TamerMain <arthur.tamer.main@gmail.com>
2026-05-09 06:41:47 +0300

    test: prepare setup with MSW and mock API responses json

--- rs-react-app/src/tests/setup.ts
+++ rs-react-app/src/tests/setup.ts
@@ -1,8 +1,36 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
+import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { setupServer } from 'msw/node';
+import { http, HttpResponse } from 'msw';
+import mockAPIResponse from '../test-utils/mockAPIResponse.json';
+import mockAPIDefaultResponse from '../test-utils/mockAPIDefaultResponse.json';
 
+export const restHandlers = [
+  http.get(
+    'https://api.scryfall.com/cards/search?q=+%28game%3Apaper%29',
+    () => {
+      return HttpResponse.json(mockAPIDefaultResponse);
+    }
+  ),
+  http.get(
+    'https://api.scryfall.com/cards/search?q=Lotus+%28game%3Apaper%29',
+    () => {
+      return HttpResponse.json(mockAPIResponse);
+    }
+  ),
+  http.get('https://api.scryfall.com/cards/search?q=*', () => {
+    return new HttpResponse(null, { status: 404 });
+  }),
+];
+
+const server = setupServer(...restHandlers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+afterAll(() => server.close());
 
 afterEach(() => {
+  server.resetHandlers();
   cleanup();
 });
