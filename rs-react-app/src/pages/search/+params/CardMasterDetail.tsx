2ae0c06a38599477024baecd7ec0011f47e17b27
TamerMain <arthur.tamer.main@gmail.com>
2026-05-18 07:06:54 +0300

    feat: error handling when fetch details

--- rs-react-app/src/pages/search/+params/CardMasterDetail.tsx
+++ rs-react-app/src/pages/search/+params/CardMasterDetail.tsx
@@ -6,7 +6,8 @@ function CardMasterDetail(props: {
   activeCard: string;
   setActiveCard: (cardName: string | undefined) => void;
 }) {
-  const { resultCard, updateResultCard, navigate, isLoading } = useFetchCard();
+  const { resultCard, updateResultCard, navigate, isLoading, isError } =
+    useFetchCard();
 
   useEffect(() => {
     updateResultCard(props.activeCard);
@@ -28,25 +29,30 @@ function CardMasterDetail(props: {
       )}
       {!isLoading && (
         <div className="fixed flex flex-col items-center w-1/4 p-2 bg-mist-800 text-center fade-in">
-          <div className=" pr-2 ">{resultCard?.set_name} Set</div>
-          <h2 className="text-2xl">{resultCard?.name}</h2>
-          <img
-            alt={`Full Art of ${resultCard?.name} Card`}
-            className="w-full max-w-95 rounded-3xl"
-            src={
-              resultCard?.image_uris?.normal ||
-              resultCard?.card_faces?.[0]?.image_uris?.normal
-            }
-            width="480"
-            height="680"
-          ></img>
-          <div className="flex flex-row gap-10"></div>
-          <button
-            className="absolute  right-0 top-0 p-2 cursor-pointer"
-            onClick={handleCloseMasterDetail}
-          >
-            X
-          </button>
+          {isError && <h2 className="text-2xl">Details not found for that card.</h2>}
+          {!isError && (
+            <>
+              <div className=" pr-2 ">{resultCard?.set_name} Set</div>
+              <h2 className="text-2xl">{resultCard?.name}</h2>
+              <img
+                alt={`Full Art of ${resultCard?.name} Card`}
+                className="w-full max-w-95 rounded-3xl"
+                src={
+                  resultCard?.image_uris?.normal ||
+                  resultCard?.card_faces?.[0]?.image_uris?.normal
+                }
+                width="480"
+                height="680"
+              ></img>
+              <div className="flex flex-row gap-10"></div>
+              <button
+                className="absolute  right-0 top-0 p-2 cursor-pointer"
+                onClick={handleCloseMasterDetail}
+              >
+                X
+              </button>
+            </>
+          )}
         </div>
       )}
     </>
