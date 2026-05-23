97ba471b8729db8c282f8b03c0c5a5a021d408b7
TamerMain <arthur.tamer.main@gmail.com>
2026-05-23 05:59:00 +0300

    feat: configure cart store for card selection

---
+++ rs-react-app/src/store/store.ts
@@ -0,0 +1,30 @@
+import {
+  configureStore,
+  createSlice,
+  type PayloadAction,
+} from '@reduxjs/toolkit';
+
+const initialState: string[] = [];
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    toggleItem: (state, action: PayloadAction<string>) => {
+      const index = state.indexOf(action.payload);
+      if (index === -1) {
+        state.push(action.payload);
+      } else {
+        state.splice(index, 1);
+      }
+    },
+    clearCart: () => initialState,
+  },
+});
+export const { toggleItem, clearCart } = cartSlice.actions;
+
+export const store = configureStore({
+  reducer: { cart: cartSlice.reducer },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
