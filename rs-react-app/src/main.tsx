dfe840a2267ba80330f50142defca8d394373e2a
TamerMain <arthur.tamer.main@gmail.com>
2026-05-23 05:59:50 +0300

    feat: add store provider

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from '@/store/store.ts';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
